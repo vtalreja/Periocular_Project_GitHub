2e39a0a366799a0480da810f53f1f81930b5c87d
Veeru Talreja <vtalreja@mix.wvu.edu>
2021-02-17 13:50:28 -0500

    Made some changes to the file for including the attribute model training

--- main_attribute_classification.py
+++ main_attribute_classification.py
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 from dataset_utils import *
 from model_utils import *
 from model import *
+from utils import *
 
 # Top level data directory. Here we assume the format of the directory conforms
 #   to the ImageFolder structure
@@ -37,59 +38,51 @@ classification = False
 # # Learning rate for optimizers
 # lr = 0.01
 # target_lr = 0.001
-lr =0.001
+lr = 0.0005
 
 num_workers = 4
 
 # Number of epochs to train for
-num_epochs = 180
+num_epochs = 70
 
 ngpu = 2
 
 split_list = ['train', 'val']
 
-restart_training = False
+restart_training = True
 
 ckpt_fname = None
 
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
-results_dir = os.path.join('Classification_Results', model_name + '_results_' + str(180) + '_epochs')
+results_dir = os.path.join('Attribute_Classification_Results', model_name + '_results_' + str(50) + '_epochs_'+str(lr))
 if not (os.path.exists(results_dir)):
     os.mkdir(results_dir)
 
-# Opening JSON file
-f = open('runtime.json', )
-
 # returns JSON object as
 # a dictionary
-data = json.load(f)
+data = json.load(open('runtime.json', 'r'))
 
 train_results_txt = os.path.join(results_dir, 'results_train_metrics.txt')
 val_results_txt = os.path.join(results_dir, 'results_val_metrics.txt')
 
-data_loader = data_utils(batch_size=batch_size,train_size=0.75,num_workers=num_workers,num_classes_sampler=2,num_samples=16,balanced_batches=True)
+data_loader = data_utils(batch_size=batch_size, train_size=0.75, num_workers=num_workers, num_classes_sampler=2,
+                         num_samples=16, balanced_batches=True)
 
 # dataloaders, dataset_sizes, class_names = data_loader.load_classification_data(data_dir, split_list)
 
-dataloaders, dataset_sizes, attribute_mapper = data_loader.load_attribute_classification_data(image_folder ='/home/n-lab/Documents/Periocular_project/Datasets/ubipr/Class_Folders_Left_Images_Split',output_folder='/home/n-lab/Documents/Periocular_Project_GitHub',attribute_data_csv_loc='attribute_data.csv')
-
-
+dataloaders, dataset_sizes, attribute_mapper = data_loader.load_attribute_classification_data(
+    image_folder='/home/n-lab/Documents/Periocular_project/Datasets/ubipr/Class_Folders_Left_Images_Split',
+    output_folder='/home/n-lab/Documents/Periocular_Project_GitHub', attribute_data_csv_loc='attribute_data.csv')
 
 # Get a batch of training data
 data_dict = next(iter(dataloaders['train']))
-indices = list(range(len(data_dict['gender_label'])))
-np.random.shuffle(indices)
-data_dict['img'] = data_dict['img'][indices]
-data_dict['gender_label'] = data_dict['gender_label'][indices]
-data_dict['class_name_label'] = data_dict['class_name_label'][indices]
-classes = data_dict['gender_label']
+new_data_dict = shuffle_dict(data_dict)
+classes = new_data_dict['gender_label']
 # Make a grid from batch
-out = torchvision.utils.make_grid(data_dict['img'])
+out = torchvision.utils.make_grid(new_data_dict['img'])
 # title_list=[attribute_mapper.gender_id_to_name[x] for x in classes]
 imshow(out, title=[attribute_mapper.gender_labels[x] for x in classes])
-
-
 
 if classification:
     if model_name == "vgg":
@@ -99,9 +92,12 @@ if classification:
     elif model_name in ["resnet", "squeezenet"]:
         """ Resnet50 or squeezenet1_0
         """
-        model = initialize_model(model_name, num_classes = attribute_mapper.num_class_names, use_pretrained=True)
-else :
-    model = attribute_model(load_model_fname='/home/n-lab/Documents/Periocular_Project_GitHub/vgg_results_70epochs/epoch_68.pth', model_name=model_name,num_classes_classification=attribute_mapper.num_class_names,use_pretrained=True,head_config=data['head_configs'][0])
+        model = initialize_model(model_name, num_classes=attribute_mapper.num_class_names, use_pretrained=True)
+else:
+    model = attribute_model(
+        load_model_fname='/home/n-lab/Documents/Periocular_Project_GitHub/vgg_results_70epochs/epoch_68.pth',
+        model_name=model_name, num_classes_classification=attribute_mapper.num_class_names, use_pretrained=True,
+        head_config=data['head_configs'][0])
 
 # BCE loss and Adam optimizer
 criterion = nn.CrossEntropyLoss()
@@ -128,7 +124,8 @@ if (restart_training):
     else:
         latest_ckpt = os.path.join(results_dir, ckpt_fname)
     assert latest_ckpt != None, ' latest_ckpt cannot be None as None cannot be loaded in the model'
-    model, optimizer, start_epoch, train_metrics, val_metrics,exp_lr_scheduler = load_ckp(latest_ckpt, model, optimizer,exp_lr_scheduler)
+    model, optimizer, start_epoch, train_metrics, val_metrics, exp_lr_scheduler = load_ckp(latest_ckpt, model,
+                                                                                           optimizer, exp_lr_scheduler)
     print("Last epoch on saved checkpoint is {}. Training will start from {} for more {} iterations".format(
         start_epoch - 1, start_epoch, num_epochs - start_epoch - 1))
 
@@ -139,13 +136,14 @@ if (restart_training):
 print(model)
 
 # Training Loop
-model, train_metrics, val_metrics = train_model(start_epoch=start_epoch, model=model, dataloaders=dataloaders,
-                                                criterion=criterion, optimizer=optimizer, scheduler=exp_lr_scheduler,
-                                                num_epochs=num_epochs, device=device, results_dir=results_dir,
-                                                train_metrics=train_metrics, val_metrics=val_metrics,
-                                                is_inception=False)
+model, train_metrics, val_metrics = train_model_attribute(start_epoch=start_epoch, model=model, dataloaders=dataloaders,
+                                                          criterion=criterion, optimizer=optimizer,
+                                                          scheduler=exp_lr_scheduler,
+                                                          num_epochs=num_epochs, device=device, results_dir=results_dir,
+                                                          train_metrics=train_metrics, val_metrics=val_metrics,head_config=data['head_configs'][0],
+                                                          is_inception=False)
 
-visualize_model(model, device, dataloaders, attribute_mapper.gender_labels, num_images=6)
+visualize_model_attribute(model, device, dataloaders, attribute_mapper.gender_labels, data['head_configs'][0], num_images=6)
 print(train_metrics)
 print(val_metrics)
 

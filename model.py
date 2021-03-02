1b5be55c39501d0d25cb40adeb0ca41d3d6d8753
Veeru Talreja <vtalreja@mix.wvu.edu>
2021-03-01 22:13:04 -0500

    Added the joint model and joint model training loop

--- model.py
+++ model.py
@@ -54,10 +54,16 @@ class attribute_model(nn.Module):
                 param.requires_grad = False
             self.features = model_ft.features
             self.classifier = model_ft.classifier
-            self.classifier[3] = nn.Linear(2048, 512)
-            self.classifier[4] = nn.ReLU(True)
-            self.classifier[5] = nn.Dropout()
-            self.classifier[6] = nn.Linear(512, 256)
+            self.attribute_classifier = nn.Sequential(
+                nn.Linear(2048, 512),
+                nn.ReLU(True),
+                nn.Dropout(),
+                nn.Linear(512, 256))
+            # self.attribute_classifier = model_ft.classifier
+            # self.attribute_classifier[3] = nn.Linear(2048, 512)
+            # self.attribute_classifier[4] = nn.ReLU(True)
+            # self.attribute_classifier[5] = nn.Dropout()
+            # self.attribute_classifier[6] = nn.Linear(512, 256)
 
         # Define the heads attribute as a Module List as it will be easier to dynamically
         # assign the number of heads as nn.Linear modules depending on the configuration
@@ -92,7 +98,10 @@ class attribute_model(nn.Module):
         x = self.features(x)
         # x = self.avgpool(x)
         x = torch.flatten(x, 1)
-        x = self.classifier(x)
+        x = self.classifier[0](x)
+        x = self.classifier[1](x)
+        x = self.classifier[2](x)
+        x = self.attribute_classifier(x)
 
         output_dict = {}
 
@@ -101,6 +110,155 @@ class attribute_model(nn.Module):
             output_dict[name] = self.heads[i](x)
 
         return output_dict
+
+class joint_optimization_model(nn.Module):
+    def __init__(self, load_model_fname, load_model_attribute_name,model_name, num_classes_classification, use_pretrained, head_config):
+        super(joint_optimization_model, self).__init__()
+        if model_name == "vgg":
+            model_ft = attribute_model(load_model_fname, model_name, num_classes_classification, use_pretrained, head_config)
+            # model_ft = classification_model(model_name, num_classes_classification, use_pretrained)
+            # original saved file with DataParallel
+            state_dict = torch.load(load_model_attribute_name)
+
+            # create new OrderedDict that does not contain `module.`
+            new_state_dict = OrderedDict()
+            for k, v in state_dict['state_dict'].items():
+                name = k[7:]  # remove `module.`
+                new_state_dict[name] = v
+
+            # load params
+            # model_ft.load_state_dict(state_dict)
+            model_ft.load_state_dict(new_state_dict)
+            for param in model_ft.features.parameters():
+                param.requires_grad = False
+            for param in model_ft.classifier.parameters():
+                param.requires_grad = True
+
+
+            # for param in model_ft.parameters():
+            #     param.requires_grad = False
+            self.features = model_ft.features
+            self.classifier = model_ft.classifier
+            self.attribute_classifier = model_ft.attribute_classifier
+            self.heads = model_ft.heads
+            self.concat_fc = nn.Sequential(
+                nn.Linear(1024 + 256, 512),
+                nn.ReLU(True),
+                nn.Dropout())
+            self.final_classifier = nn.Linear(512, num_classes_classification)
+            self.head_config = head_config
+            # self.attribute_classifier = model_ft.classifier
+            # self.attribute_classifier[3] = nn.Linear(2048, 512)
+            # self.attribute_classifier[4] = nn.ReLU(True)
+            # self.attribute_classifier[5] = nn.Dropout()
+            # self.attribute_classifier[6] = nn.Linear(512, 256)
+
+
+    def _create_head(self, in_features, num_of_labels):
+        '''A function to creat the individual heads as a sequential model
+
+        Args:
+        in_features (int): The number of input features for the linear layer of the head
+        num_of_labels (int): The number of labels for the given head, which will also be the number of output features for the linear layer
+
+
+        Returns:
+        A nn.Linear layer, which corresponds to a single head of the multi-head classifier
+
+        '''
+        return nn.Linear(in_features=in_features, out_features=num_of_labels)
+
+    def forward(self, x):
+        x = self.features(x)
+        # x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier[0](x)
+        x = self.classifier[1](x)
+        x_bottleneck = self.classifier[2](x)
+        x_class = self.classifier[3](x_bottleneck)
+
+
+        x_attr = self.attribute_classifier(x_bottleneck)
+
+        concatenated_x = torch.cat((x_class,x_attr),dim=1)
+
+        out_concat_layer = self.concat_fc(concatenated_x)
+        class_output = self.final_classifier(out_concat_layer)
+        output_dict = {}
+
+        for i, (name, num_classes) in enumerate(
+                self.head_config.items()):  # Enumerate through the head config to propagate the flattened tensor through the head classifiers
+            output_dict[name] = self.heads[i](x_attr)
+
+        return output_dict,class_output
+
+
+# class attribute_model(nn.Module):
+#     def __init__(self, load_model_fname, model_name, num_classes_classification, use_pretrained, head_config):
+#         super(attribute_model, self).__init__()
+#         if model_name == "vgg":
+#             model_ft = classification_model(model_name, num_classes_classification, use_pretrained)
+#             # original saved file with DataParallel
+#             state_dict = torch.load(load_model_fname)
+#             # create new OrderedDict that does not contain `module.`
+#             new_state_dict = OrderedDict()
+#             for k, v in state_dict.items():
+#                 name = k[7:]  # remove `module.`
+#                 new_state_dict[name] = v
+#             # load params
+#             # model.load_state_dict(new_state_dict)
+#             model_ft.load_state_dict(new_state_dict)
+#             for param in model_ft.parameters():
+#                 param.requires_grad = False
+#             self.features = model_ft.features
+#             self.classifier = model_ft.classifier
+#             self.classifier[3] = nn.Linear(2048, 512)
+#             self.classifier[4] = nn.ReLU(True)
+#             self.classifier[5] = nn.Dropout()
+#             self.classifier[6] = nn.Linear(512, 256)
+#
+#         # Define the heads attribute as a Module List as it will be easier to dynamically
+#         # assign the number of heads as nn.Linear modules depending on the configuration
+#         self.heads = nn.ModuleList()
+#
+#         # Defining this will help to get the keys name in the forward function
+#         self.head_config = head_config
+#
+#         self.Softmax = nn.Softmax(dim=1)  # Softmax to be used for evaluation
+#
+#         # Append each head as a nn.Linear model
+#         for head_name, num_label in (head_config.items()):
+#             h = self._create_head(256, num_label)
+#             self.heads.append(h)
+#         # self.heads.to(self.device)
+#
+#     def _create_head(self, in_features, num_of_labels):
+#         '''A function to creat the individual heads as a sequential model
+#
+#         Args:
+#         in_features (int): The number of input features for the linear layer of the head
+#         num_of_labels (int): The number of labels for the given head, which will also be the number of output features for the linear layer
+#
+#
+#         Returns:
+#         A nn.Linear layer, which corresponds to a single head of the multi-head classifier
+#
+#         '''
+#         return nn.Linear(in_features=in_features, out_features=num_of_labels)
+#
+#     def forward(self, x):
+#         x = self.features(x)
+#         # x = self.avgpool(x)
+#         x = torch.flatten(x, 1)
+#         x = self.classifier(x)
+#
+#         output_dict = {}
+#
+#         for i, (name, num_classes) in enumerate(
+#                 self.head_config.items()):  # Enumerate through the head config to propagate the flattened tensor through the head classifiers
+#             output_dict[name] = self.heads[i](x)
+#
+#         return output_dict
 
 
 def initialize_model(model_name, num_classes, use_pretrained=True):
--- model_utils.py
+++ model_utils.py
@@ -15,6 +15,11 @@ import copy
 
 from dataset_utils import *
 from utils import *
+from sklearn.metrics import (
+    confusion_matrix,
+    ConfusionMatrixDisplay
+)
+
 
 
 def train_model(start_epoch, model, dataloaders, criterion, optimizer, scheduler, num_epochs, device, results_dir,
@@ -374,3 +379,224 @@ def visualize_model_attribute(model, device, dataloaders, class_names, head_conf
                     return
         model.train(mode=was_training)
 
+def train_model_joint_optim(start_epoch, model, dataloaders, criterion, optimizer, scheduler, num_epochs, device, results_dir,
+                train_metrics, val_metrics,head_config,
+                is_inception=False):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    updated_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    # best_train_metrics = {'loss': [], 'acc': []}
+    # best_val_metrics = {'loss': [], 'acc': []}
+    for epoch in range(start_epoch, num_epochs + 1):
+        print('Epoch {}/{}'.format(epoch, num_epochs))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            gender_running_corrects = {head_name: 0 for head_name, _ in head_config.items()}
+            class_running_corrects = 0
+
+            # Iterate over data.
+            for data_dict in dataloaders[phase]:
+                shuffled_data_dict = shuffle_dict(data_dict)
+                inputs = shuffled_data_dict['img']
+                gender_labels = {name: shuffled_data_dict[name] for name,_ in head_config.items()}
+                class_labels = shuffled_data_dict['class_name_label']
+                inputs = inputs.to(device)
+                for name, _ in head_config.items():
+                    gender_labels[name] = gender_labels[name].to(device)
+                class_labels = class_labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4 * loss2
+                    else:
+                        outputs,class_outputs = model(inputs)
+                        calc = calculations(head_config=head_config, device=device, outputs=outputs, gts=gender_labels)
+                        gender_loss = calc.calculate_loss(criterion=criterion)
+                        class_loss = criterion(class_outputs,class_labels)
+                        loss = class_loss + 1.0 * gender_loss
+
+                        # loss = criterion(outputs, labels)
+                    gender_preds = calc.calculate_predictions()
+                    _, class_preds = torch.max(class_outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                gender_running_corrects = calc.calculate_running_corrects(preds = gender_preds, running_corrects=gender_running_corrects)
+                class_running_corrects += torch.sum(class_preds == class_labels.data)
+
+                # running_corrects += torch.sum(preds == labels.data)
+            # if phase == 'train':
+            #     scheduler.step()
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            gender_epoch_acc = calc.calculate_acc(running_corrects=gender_running_corrects,dataloaders=dataloaders[phase])
+            class_epoch_acc = class_running_corrects.double() / len(dataloaders[phase].dataset)
+            epoch_acc = (gender_epoch_acc+class_epoch_acc)/2
+
+            print('{} Loss: {:.4f} Avg_Acc: {:.4f} Gender_Acc: {:.4f} Class_Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc,gender_epoch_acc,class_epoch_acc))
+            if phase == 'train':
+                train_metrics['loss'].append(epoch_loss)
+                train_metrics['class_acc'].append(class_epoch_acc)
+                train_metrics['gender_acc'].append(gender_epoch_acc)
+
+            else:
+                val_metrics['loss'].append(epoch_loss)
+                val_metrics['class_acc'].append(class_epoch_acc)
+                val_metrics['gender_acc'].append(gender_epoch_acc)
+            # deep copy the model
+            if phase == 'val' and class_epoch_acc > best_acc:
+                best_acc = class_epoch_acc
+                best_epoch = epoch
+                best_model_wts = copy.deepcopy(model.state_dict())
+                best_optimizer_dict = copy.deepcopy(optimizer.state_dict())
+                best_scheduler = copy.deepcopy(scheduler.state_dict())
+                best_train_metrics = {x: train_metrics[x][:best_epoch] for x in ['loss', 'class_acc','gender_acc']}
+                best_val_metrics = {x: val_metrics[x][:best_epoch] for x in ['loss', 'class_acc','gender_acc']}
+
+        if (epoch % 5 == 0):
+            updated_model_wts = copy.deepcopy(model.state_dict())
+            updated_optimizer_dict = copy.deepcopy(optimizer.state_dict())
+            updated_scheduler = copy.deepcopy(scheduler.state_dict())
+            save_ckp(save_dir=results_dir, epoch=epoch, state_dict=updated_model_wts, optimizer=updated_optimizer_dict,
+                     train_metrics=train_metrics, val_metrics=val_metrics, scheduler = updated_scheduler)
+            # torch.save(updated_model_wts, os.path.join(results_dir, 'epoch_' + str(epoch) + '.pth'))
+
+        # adjust_learning_rate(optimizer, lr, target_lr, epoch, None, num_epochs)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best Class Acc: {:4f} , which is for epoch: {}'.format(best_acc, best_epoch))
+
+    if not os.path.exists(os.path.join(results_dir, 'epoch_' + str(
+            best_epoch) + '.pt')):  # Save the best model only if it has not been saved previously in the regular updates
+        save_ckp(save_dir=results_dir, epoch=best_epoch, state_dict=best_model_wts, optimizer=best_optimizer_dict,
+                 train_metrics=best_train_metrics, val_metrics=best_val_metrics, scheduler = best_scheduler)
+        # torch.save(best_model_wts, os.path.join(results_dir, 'epoch_' + str(best_epoch) + '.pth'))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, train_metrics, val_metrics
+
+
+def visualize_model_joint_optim(model, device, dataloaders, attribute_mapper, head_config,results_dir,num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+    predicted_gender_all=[]
+    gt_gender_all=[]
+    predicted_class_all =[]
+    gt_class_all =[]
+
+    with torch.no_grad():
+        for i, data_dict in enumerate(dataloaders['val']):
+            shuffled_data_dict = shuffle_dict(data_dict)
+            inputs = shuffled_data_dict['img']
+            gender_labels = {name: shuffled_data_dict[name] for name, _ in head_config.items()}
+            class_labels = shuffled_data_dict['class_name_label']
+            inputs = inputs.to(device)
+            for name, _ in head_config.items():
+                gender_labels[name] = gender_labels[name].to(device)
+            class_labels = class_labels.to(device)
+
+
+            outputs,class_outputs = model(inputs)
+            calc = calculations(head_config=head_config, device=device, outputs=outputs, gts=gender_labels)
+            gender_preds = calc.calculate_predictions()
+            _, class_preds = torch.max(class_outputs, 1)
+            for i in range(inputs.shape[0]):
+                # image = np.clip(img[i].permute(1, 2, 0).numpy() * std + mean, 0, 1)
+
+                predicted_gender = attribute_mapper.gender_id_to_name[gender_preds['gender_label'][i].item()]
+                predicted_class = attribute_mapper.class_id_to_name[class_preds[i].item()]
+
+                gt_gender = attribute_mapper.gender_id_to_name[gender_labels['gender_label'][i].item()]
+                gt_class = attribute_mapper.class_id_to_name[class_labels[i].item()]
+
+                gt_gender_all.append(gt_gender)
+                gt_class_all.append(gt_class)
+
+                predicted_gender_all.append(predicted_gender)
+                predicted_class_all.append(predicted_class)
+            # predicted_gender_all.extend(
+            #     prediction.item() for prediction in gender_preds
+            # )
+            # gt_gender_all.extend(
+            #     gt_color.item() for gt_color in gender_labels
+            # )
+            # predicted_class_all.extend(
+            #     prediction.item() for prediction in class_preds
+            # )
+            # gt_class_all.extend(
+            #     gt_color.item() for gt_color in class_labels
+            # )
+        plt.figure(figsize=(10,5))
+        cn_matrix = confusion_matrix(
+            y_true=gt_gender_all,
+            y_pred=predicted_gender_all,
+            labels=attribute_mapper.gender_labels,
+            normalize="true")
+        ConfusionMatrixDisplay(cn_matrix, attribute_mapper.gender_labels).plot(
+            include_values=False, xticks_rotation="vertical")
+        plt.title("Gender")
+        plt.tight_layout()
+        plt.savefig(os.path.join(results_dir, 'Gender_Confusion.png'))
+        # plt.show()
+
+        plt.figure(figsize=(30,20))
+        cn_matrix = confusion_matrix(
+            y_true=gt_class_all,
+            y_pred=predicted_class_all,
+            labels=attribute_mapper.class_name_labels,
+            normalize="true")
+        ConfusionMatrixDisplay(cn_matrix, attribute_mapper.class_name_labels).plot(
+            include_values=False, xticks_rotation="vertical")
+        plt.title("Class")
+        plt.tight_layout()
+        plt.savefig(os.path.join(results_dir, 'Class_Confusion.png'))
+        # plt.show()
+
+            # for j in range(inputs.size()[0]):
+            #     images_so_far += 1
+            #     ax = plt.subplot(num_images // 2, 2, images_so_far)
+            #     ax.axis('off')
+            #     ax.set_title('predicted: {}'.format(class_names[gender_preds['gender_label'][j]]))
+            #     imshow(inputs.cpu().data[j])
+            #
+            #     if images_so_far == num_images:
+            #         model.train(mode=was_training)
+            #         return
+    model.train(mode=was_training)
